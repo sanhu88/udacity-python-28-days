9ce3883cf3b57c6d465cdabf9b50787d50a49165
Burt Zheng <504779282@qq.com>
2019-10-21 22:35:25 +0800

    add answer of 3-26

--- 3_24-turtle练习.py
+++ 3_24-turtle练习.py
@@ -44,3 +44,30 @@ amy.pendown()
 amy.color("yellow")
 amy.forward(40)
 '''
+
+
+# 答案
+'''
+import turtle
+amy = turtle.Turtle()
+
+# Make the width thicker so that the line will be easier to see
+# 使线条宽度加粗，以便更容易看到线条
+amy.width(5)
+
+# Move back without drawing anything
+# 向后移动且不画任何东西
+amy.penup()
+amy.back(140)
+amy.pendown()
+
+# Draw three lines of different colors, with space in between
+#绘制三条不同颜色的线，线与线之间用空格间隔
+for prettycolor in ["red", "orange", "yellow"]:
+    amy.color(prettycolor)
+    amy.forward(50)
+    amy.penup()
+    if prettycolor != "yellow":
+        amy.forward(50)
+    amy.pendown()
+'''

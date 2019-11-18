e5eeeef581508cd162482e1986aa8d485884ccd7
Burt Zheng <504779282@qq.com>
2019-11-18 22:20:00 +0800

    strings and ints

---
+++ 8-11_total_of_three.py
@@ -0,0 +1,20 @@
+#未完成
+def total_of_three():
+	t = int(0)
+	for i in ['1st','2nd','3rd']:
+	  print(int(t))
+		x = input('type the '+str(i)+' int')
+		print(int(x))
+		#t = t +int(i[0])
+		#t = t + int(x)
+    #print(int(x))
+total_of_three()
+
+'''
+def total_of_three():
+   one = input("Enter a number: ")
+   two = input("Enter another number: ")
+   three = input("Enter a third number: ")
+   result = int(one) + int(two) + int(three)
+   print(f"{one} + {two} + {three} = {result}")
+'''

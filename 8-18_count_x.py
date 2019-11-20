d98ebdd3f7ad5f634381c58cbedb4000f3888e6f
Burt Zheng <504779282@qq.com>
2019-11-20 15:58:36 +0800

    traverse str and list

--- 8-18_count_x.py
+++ 8-18_count_x.py
@@ -5,3 +5,13 @@ def count_x(str):
 			counter += 1
 	print("X or x show ",counter," times.")
 count_x("oxen and foxen all live in boxen")
+
+#答案
+'''
+def count_ch(string, target):
+    total = 0
+    for ch in string:
+        if ch == target:
+            total += 1
+    return total
+'''
--- 8-19_count_ch.py
+++ 8-19_count_ch.py
@@ -1,3 +1,4 @@
+#以下未完成，任意字符检测
 def count_ch(str,chart):
 	counter = 0
 	lenth_chart = len(chart)
@@ -7,4 +8,8 @@ def count_ch(str,chart):
 	print(chart,"show ",counter," times in",str)
 count_ch("oxen and foxen all live in boxen","li")		
 
+#count 函数 直接返回
+def count_any(str,chart):
+	print(chart,"show ",str.count(chart)," time(s) in",str)
+count_any("oxen and foxen all live in boxen","li")
 
